987cd06a917e15d1d3a2d940b172bdd1b0e3cd76
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-10-27 01:48:46 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const buttons = document.querySelectorAll(".buttons button");
+const scoreNumber = document.querySelector("#scoreNumber");
+
+const generateNumber = () => Math.floor(Math.random() * 256);
+console.log(generateNumber());
